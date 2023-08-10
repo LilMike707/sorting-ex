481f6d81d154c210b4b7eb7174f70cb0c0a99520
LilMike707 <michaelmarracq@yahoo.com>
2023-08-09 17:52:55 -0700

    done

---
+++ bubble.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+  const len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+module.exports = bubbleSort;
